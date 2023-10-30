ee8f880bf90450568c29d124eaedd64bbe4751e5
stealcash <pawansaini312@gmail.com>
2023-10-30 23:37:18 +0530

    add config

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = "";
+export const SITE_BASE_PATH = "/dev/";
+export const IN_MAINTENANCE_MODE = false;
+export const MAINTENANCE_MESSAGE =
+  "We are doing important maintenance work on the website and will be back shortly.";
